ec06321eb0ba71df0a5e5fb4f6ca7251973cdcae
mibrahim <politokuet2k2@gmail.com>
2019-08-07 00:14:59 +0200

    First Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+//import HookMouse from './components/HookMouse';
+//import MouseContainer from './components/MouseContainer';
+import DataFetchingId from './components/DataFetchingId';
+function App() {
+  return (
+    <div className="App">
+    {/*<HookMouse />*/}
+    {/*<MouseContainer />*/}
+    <DataFetchingId />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DataFetching.js
@@ -0,0 +1,28 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+function DataFetching(){
+	const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		axios.get('https://jsonplaceholder.typicode.com/posts')
+		     .then(res => {
+		     	console.log(res);
+		     	setPosts(res.data);
+		     })
+		     .catch(err => {
+		     	console.log(err);
+		     })
+	}, []);	
+  return (
+    <div>
+    <ol>
+      {
+      	posts.map(post => (<li key = {post.id}>{post.title}<br/>{post.body}</li>))
+      }
+    </ol>
+    </div>
+  )
+}
+
+export default DataFetching;
